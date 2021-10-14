9050885e9e8a73ce453c543bdc3cda666a4aa8f8
Ryan Tam <ryanjtam@gmail.com>
2021-10-14 13:48:18 -0700

    added more functionality. adding meal and deleting meals

--- app.js
+++ app.js
@@ -18,21 +18,20 @@ let thisArray = [
 
 ]
 
-console.log(thisArray)
+// console.log(thisArray)
 
-localStorage.setItem('hi', JSON.stringify(thisArray))
+// localStorage.setItem('hi', JSON.stringify(thisArray))
 
 console.log([localStorage['hi']])
 
-let choosen = thisArray[Math.floor(Math.random() * thisArray.length)]
+// let choosen = thisArray[Math.floor(Math.random() * thisArray.length)]
 
-console.log(choosen)
+// console.log(choosen)
 
 let newMeal = document.getElementById('addmeal')
 let doc = document.getElementById('doc')
 
 console.log(newMeal.value)
-console.log(doc)
 
 //when button is clicked, push the value of the addmeal to the array
 //
@@ -50,6 +49,12 @@ addBtn.addEventListener('click', () => {
         newMeal.value = ''
 
         displayAll.appendChild(document.createElement('br'))
+        
+        // reload for now
+        window.location.reload()
+        // without it loading, I need to create a span with an ID following the last span. 
+
+
 
     } else {
         alert('There is no meal to add in text field.')
@@ -60,7 +65,42 @@ addBtn.addEventListener('click', () => {
 
 for (let i = 0; i < JSON.parse(localStorage.getItem('hi')).length; i++) {
     let displayArray = JSON.parse(localStorage.getItem('hi'))
-    displayAll.append(displayArray[i])
+    let x = document.createElement(`a`)
+    x.append(`[x]`)
+    x.setAttribute('id', `${i}`)
+    x.setAttribute('href', ``)
+    x.setAttribute('class', `delete-dinner`)
+    displayAll.append(x, displayArray[i])
+    // document.getElementById(`dinner${i}`).append(displayArray[i])
     displayAll.appendChild(document.createElement('br'))
 
 }
+
+let deleteMeal = document.getElementsByClassName('delete-dinner')
+
+for (let i = 0; i < deleteMeal.length; i++) {
+    console.log(deleteMeal[i])
+    deleteMeal[i].addEventListener('click', (e) => {
+        
+        let deleteId = e.path[0].getAttribute('id')
+        console.log(deleteId)
+        let newArray = JSON.parse(localStorage.getItem('hi'))
+        console.log(newArray)
+        newArray.splice(deleteId, 1)
+        console.log(newArray)
+        localStorage.setItem('hi', JSON.stringify(newArray))
+        window.location.reload()
+    }, false)
+}
+
+
+
+console.log(deleteMeal)
+
+// deleteMeal.addEventListener('click', (e) => {
+//     let deleteId = e.path[0].getAttribute('id')
+//     console.log(e.path[0].getAttribute('id'))
+//     console.log(e.path)
+
+//     JSON.parse(localStorage.getItem('hi')).splice(deleteId, deleteId)
+// })
